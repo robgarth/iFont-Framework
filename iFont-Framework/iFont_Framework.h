977e551ab20310a031f63d9fd0badf57e63d3a97
robertgarth <rob.garth13@icloud.com>
2025-01-21 20:49:51 -0800

    RCG

---
+++ iFont-Framework/iFont_Framework.h
@@ -0,0 +1,18 @@
+//
+//  iFont_Framework.h
+//  iFont-Framework
+//
+//  Created by Rob Garth on 1/21/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iFont_Framework.
+FOUNDATION_EXPORT double iFont_FrameworkVersionNumber;
+
+//! Project version string for iFont_Framework.
+FOUNDATION_EXPORT const unsigned char iFont_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iFont_Framework/PublicHeader.h>
+
+
